481749dc0879b7a77de52d8f732155bb076ba457
Niña Gillian Villamin <124445105+NoobNumi@users.noreply.github.com>
2025-05-18 12:19:41 +0800

    fix: Standardize quotes and formatting in vite.config.js

--- client_app/vite.config.js
+++ client_app/vite.config.js
@@ -1,10 +1,15 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   server: {
-    host: '0.0.0.0', // Allow access from external devices
-    port: 5173 // Ensure this is the correct port
+    host: "0.0.0.0", // Allow access from external devices
+    port: 5173, // Ensure this is the correct port
   },
-  plugins: [{ enforce: 'pre' }, react({ include: /\.(js|jsx|ts|tsx)$/ })]
+  build: {
+    outDir: "./", // Output directory for the build
+    emptyOutDir: true, // Clear the output directory before building
+    sourcemap: true, // Generate source maps for easier debugging
+  },
+  plugins: [{ enforce: "pre" }, react({ include: /\.(js|jsx|ts|tsx)$/ })],
 });
